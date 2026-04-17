1dff2db0c479690c7203d66cd6a1220e09a07467
talentestors <i@yuhiri.me>
2026-04-17 14:16:11 +0800

    learn complex

--- complex-num/src/main.rs
+++ complex-num/src/main.rs
@@ -1,3 +1,12 @@
+use num::complex::Complex;
+
 fn main() {
-    println!("Hello, world!");
+    let a = Complex { re: 2.1, im: -1.2 };
+    let b: Complex<f64> = Complex::new(11.1, 22.2);
+    let result: Complex<f64> = a + b;
+
+    println!("{} + {}i", result.re, result.im);
+
+    let i: Complex<i32> = Complex::i();
+    println!("{}", i);
 }
