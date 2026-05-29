40eb8246fe060c5dce6469e64aa779027bbef3b5
talentestors <i@yuhiri.me>
2026-05-29 16:02:54 +0800

    basic done

---
+++ basic-practice/minigrep/src/main.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    // let args: Vec<String> = env::args().collect();
+
+    // // 对 build 返回的 `Result` 进行处理
+    // let config = Config::build(&args).unwrap_or_else(|err| {
+    //     eprintln!("Problem parsing arguments: {err}");
+    //     process::exit(1);
+    // });
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for '{}'", config.query);
+    println!("In file '{}'", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1)
+    };
+}
