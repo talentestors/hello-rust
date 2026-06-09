2b33e673f589c76ff1df2e1eb6f7a3e916e73cfd
talentestors <i@yuhiri.me>
2026-06-09 15:23:59 +0800

    test

---
+++ test/src/lib.rs
@@ -0,0 +1,66 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq; // 该包仅能用于测试
+
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+
+    use super::*;
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Sunface");
+        let target = "孙飞";
+        assert!(
+            result.contains(target),
+            "你的问候中并没有包含目标姓名 {} ，你的问候是 `{}`",
+            target,
+            result
+        );
+    }
+
+    #[test]
+    // #[should_panic]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+}
