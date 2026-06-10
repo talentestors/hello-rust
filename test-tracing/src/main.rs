c93fc93d17384a4aff5b10e7407eb8cdd2c8bc6a
talentestors <i@yuhiri.me>
2026-06-10 17:05:52 +0800

    custom_layer 3

--- test-tracing/src/main.rs
+++ test-tracing/src/main.rs
@@ -7,8 +7,10 @@ use custom_layer::CustomLayer;
 fn main() {
     tracing_subscriber::registry().with(CustomLayer).init();
 
-    let outer_span = info_span!("outer", level = 0);
+    let outer_span = info_span!("outer", level = 0, other_field = tracing::field::Empty);
     let _outer_entered = outer_span.enter();
+    // Some code...
+    outer_span.record("other_field", &7);
 
     let inner_span = debug_span!("inner", level = 1);
     let _inner_entered = inner_span.enter();
