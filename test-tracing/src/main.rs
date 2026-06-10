d32c5b07f4d6455f53f04d5d12c876623c10fbb3
talentestors <i@yuhiri.me>
2026-06-10 16:47:58 +0800

    custom_layer

---
+++ test-tracing/src/main.rs
@@ -0,0 +1,15 @@
+// in examples/figure_0/main.rs
+
+use tracing::info;
+use tracing_subscriber::prelude::*;
+
+mod custom_layer;
+use custom_layer::CustomLayer;
+
+fn main() {
+    // 设置 `tracing-subscriber` 对 tracing 数据的处理方式
+    tracing_subscriber::registry().with(CustomLayer).init();
+
+    // 打印一条简单的日志。用 `tracing` 的行话来说，`info!` 将创建一个事件
+    info!(a_bool = true, answer = 42, message = "first example");
+}
