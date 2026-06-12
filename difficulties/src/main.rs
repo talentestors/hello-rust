e26f7b357c7613e8385595efc3a53bc96d2966df
talentestors <i@yuhiri.me>
2026-06-12 13:50:29 +0800

    first list

--- difficulties/src/main.rs
+++ difficulties/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let s1: &str = "banana";
-    let s2: &str = &String::from("banana");
+    let _s1: &str = "banana";
+    let _s2: &str = &String::from("banana");
 
     let arr = [1, 2, 3, 4, 5];
 
-    let s3: &[i32] = &arr[1..3];
+    let _s3: &[i32] = &arr[1..3];
 }
