0d86f95f8e218a135b1efe97e1b9ef5cdebc16ca
talentestors <i@yuhiri.me>
2026-06-20 16:41:45 +0800

    :tada: rust done!

---
+++ other/move.rs
@@ -0,0 +1,56 @@
+struct LargeArray {
+    a: Box<[i128; 10000]>,
+}
+
+impl LargeArray {
+    #[inline(always)]
+    fn transfer(mut self) -> Self {
+        println!("{:?}", &mut self.a[1] as *mut i128);
+
+        //do some stuff to alter it
+        self.a[1] += 23;
+        self.a[4] += 24;
+
+        //return the same object
+        self
+    }
+}
+
+fn main() {
+    let mut f = LargeArray {
+        a: Box::new([10i128; 10000]),
+    };
+
+    println!("{:?}", &mut f.a[1] as *mut i128);
+
+    let mut f2 = f.transfer();
+
+    println!("{:?}", &mut f2.a[1] as *mut i128);
+}
+// struct LargeArray {
+//     a: [i128; 10000],
+// }
+
+// impl LargeArray {
+//     #[inline(always)]
+//     fn transfer(mut self) -> Self {
+//         println!("{:?}", &mut self.a[1] as *mut i128);
+
+//         // 改变数组中的值
+//         self.a[1] += 23;
+//         self.a[4] += 24;
+
+//         // 返回所有权
+//         self
+//     }
+// }
+
+// fn main() {
+//     let mut f = LargeArray { a: [10i128; 10000] };
+
+//     println!("{:?}", &mut f.a[1] as *mut i128);
+
+//     let mut f2 = f.transfer();
+
+//     println!("{:?}", &mut f2.a[1] as *mut i128);
+// }
